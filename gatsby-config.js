5084109c6587b991a9a74c9b4997e696b69ea9f7
br33z3p1ckf0rd <151497912+br33z3p1ckf0rd@users.noreply.github.com>
2023-11-19 22:27:16 -0800

    my name and info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Breeze Pickford',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Breeze Pickford is an aspiring software engineer / sophomore USC student who builds inclusive innovative products and digital experiences.',
+    siteUrl: 'https://breezepickford.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    //I dont have twitter ... twitterUsername: '@breezepickford',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Breeze Pickford',
+        short_name: 'Breeze Pickford',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
